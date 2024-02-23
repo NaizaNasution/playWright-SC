c3946eeaa91ade031f3040dbf4abedb99a330503
John <johnlooi99@gmail.com>
2024-02-23 17:35:27 +0800

    Add test 'Create and get 'Maintenance Form Details' url' in DigitalForm.spec.ts

--- tests/Live/Dashboard/DigitalForm.spec.ts
+++ tests/Live/Dashboard/DigitalForm.spec.ts
@@ -73,3 +73,42 @@ test ('Create and get "Add Maintenance Form" url', async ({page}) => {
         await expect(pageMF).toHaveURL('https://salesconnection.my/ServiceReport/Transaction?g=1245&t=DR05&a=2477940&c=291640&d=255356&f=activity');
     });
 });
+
+test ('Create and get "Maintenance Form Details" url', async ({ page }) => {
+    await test.step('1. Go to "Add Maintenance Form"', async () => {
+        await loginAdmin(page);
+        await page.goto(addMFUrl);
+    });
+    
+    await test.step('2. Fill in details', async () => {
+        const customT1 = 'Test add MF';
+
+        await page.getByPlaceholder('Enter Custom Text 1').fill(customT1);
+    });
+
+    await test.step('3. Save the Maintenance Form', async () => {
+        await page.locator('.btn-savechanges').click();
+        await page.waitForTimeout(3000);
+        await page.getByRole('button', { name: 'OK' }).click();
+        await page.waitForTimeout(3000);
+
+        // Expect the page have shown 'Successfully Saved'
+        await expect(page.getByRole('heading', { name: 'Successfully Saved.' })).toBeVisible();
+    
+        await page.getByRole('button', { name: 'OK' }).click();
+    });
+
+    await test.step('4. Get "Maintenance Form Details" url', async () => {
+        detailMFUrl = page.url();
+
+        await test.step('Check "Maintenance Form Details" header', async () => {
+            const headerDF = page.getByRole('heading', { name: 'Maintenance Form Details' });
+
+            // Expect the page have header "Maintenance Form Details"
+            await expect(headerDF).toBeVisible();
+        });
+
+        // Expect the page not have 'Add Maintenance Form' url;
+        await expect(page).not.toHaveURL('https://salesconnection.my/ServiceReport/Transaction?g=1245&t=DR05&a=2477940&c=291640&d=255356&f=activity');
+    });
+});
